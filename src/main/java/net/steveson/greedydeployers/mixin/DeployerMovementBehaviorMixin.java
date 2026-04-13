bb4a252e41504ce776ddfbd7d5989594f262f0e4
Steve-Steveson <mr.steve.steveson@gmail.com>
2026-04-13 16:01:14 -0400

    Initial commit

---
+++ src/main/java/net/steveson/greedydeployers/mixin/DeployerMovementBehaviorMixin.java
@@ -0,0 +1,21 @@
+package net.steveson.greedydeployers.mixin;
+
+import com.simibubi.create.content.contraptions.behaviour.MovementContext;
+import com.simibubi.create.content.kinetics.deployer.DeployerMovementBehaviour;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(DeployerMovementBehaviour.class)
+public class DeployerMovementBehaviorMixin {
+
+//    public DeployerMovementBehaviorMixin() {
+//
+//    }
+
+    @Inject(method = "tryGrabbingItem", at = @At(value = "TAIL"))
+    private void tryGrabbingItem(MovementContext context, CallbackInfo ci) {
+
+    }
+}
