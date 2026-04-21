0d04901fe4ef04dae1bd64abdfc4995c0e5a68f3
Steve-Steveson <mr.steve.steveson@gmail.com>
2026-04-21 15:47:14 -0400

    added new tag checks to mixin code fixed tag contents to not require items from mods other than create minor version number increased because I did a test build

--- src/main/java/net/steveson/greedydeployers/mixin/DeployerMovementBehaviorMixin.java
+++ src/main/java/net/steveson/greedydeployers/mixin/DeployerMovementBehaviorMixin.java
@@ -21,11 +21,25 @@ public class DeployerMovementBehaviorMixin {
     private void tryGrabbingItem(MovementContext context, CallbackInfo ci, DeployerFakePlayer player) {
         FilterItemStack filter = context.getFilterFromBE();
         //Checking the filter item for the correct tag is done first so that other items don't run unnecessary code.
+        int target = 1;
+        if (Config.grabVaults && filter.item().is(ModTags.Items.DEPLOYER_WANTS_VAULTLIKE_AMOUNT)) {
+            target = Config.desiredStackSizeVaultlike;
+        } else
+        if (filter.item().is(ModTags.Items.DEPLOYER_WANTS_LARGE_AMOUNT)) {
+            target = Config.desiredStackSizeMedium;
+        } else
+        if (filter.item().is(ModTags.Items.DEPLOYER_WANTS_MEDIUM_AMOUNT)) {
+            target = Config.desiredStackSizeMedium;
+        } else
         if (filter.item().is(ModTags.Items.DEPLOYER_WANTS_SMALL_AMOUNT)) {
+            target = Config.desiredStackSizeSmall;
+        }
 
+
+        if (target > 1) {
             ItemStack itemStack = player.getMainHandItem();
             //determines how many items need to be grabbed to reach the number specified by the config file
-            int desire = Config.desiredStackSizeSmall - itemStack.getCount();
+            int desire = target - itemStack.getCount();
 
             //ignore cases where held item cannot stack with filter item, or it has enough items already
             if (ItemStack.isSameItemSameTags(filter.item(), itemStack) && desire > 0) {
